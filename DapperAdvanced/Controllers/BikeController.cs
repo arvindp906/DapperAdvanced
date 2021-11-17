be5c1089fddc82f7a987f271c17f1ad7d04bfb78
arvindp906 <arvindprajapti7523@gmail.com>
2021-11-17 18:57:22 +0530

    Add project files.

---
+++ DapperAdvanced/Controllers/BikeController.cs
@@ -0,0 +1,122 @@
+﻿using DapperAdvanced.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DapperAdvanced.Controllers
+{
+    public class BikeController : Controller
+    {
+        // GET: Bike
+        private BikesRepository repository;
+
+        public BikeController()
+        {
+            repository = new BikesRepository();
+        }
+        public ActionResult Index(RequestModel request)
+        {
+            if (request.OrderBy == null)
+            {
+                request = new RequestModel
+                {
+                    Search = request.Search,
+                    OrderBy = "name",
+                    IsDescending = false
+                };
+            }
+            ViewBag.Request = request;
+            return View(repository.GetAll(request));
+        }
+
+        // GET: Bike/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(repository.Get(id));
+        }
+
+        // GET: Bike/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bike/Create
+        [HttpPost]
+        public ActionResult Create(Bike bike, bool editAfterSaving = false)
+        {
+            if (ModelState.IsValid)
+            {
+                var lastInsertedId = repository.Create(bike);
+                if (lastInsertedId > 0)
+                {
+                    TempData["Message"] = "Record added successfully";
+                }
+                else
+                {
+                    TempData["Error"] = "Failed to add record";
+                }
+                if (editAfterSaving)
+                {
+                    return RedirectToAction("Edit", new { Id = lastInsertedId });
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
+
+        // GET: Bike/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(repository.Get(id));
+
+        }
+
+        // POST: Bike/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Bike bike)
+        {
+            if (ModelState.IsValid)
+            {
+                var recordAffected = repository.Update(bike);
+                if (recordAffected > 0)
+                {
+                    TempData["Message"] = "Record updated successfully";
+                }
+                else
+                {
+                    TempData["Error"] = "Failed to update record";
+                }
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // GET: Bike/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(repository.Get(id));
+        }
+
+        // POST: Bike/Delete/5
+        [HttpPost]
+        public ActionResult Delete(Bike bike)
+        {
+            var recordAffected = repository.Delete(bike.Id);
+            if (recordAffected > 0)
+            {
+                TempData["Message"] = "Record deleted successfully";
+            }
+            else
+            {
+                TempData["Error"] = "Failed to delete record";
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DapperAdvanced/Models/BikesRepository.cs
@@ -0,0 +1,79 @@
+﻿using Dapper;
+using Glimpse.AspNet.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+
+namespace DapperAdvanced.Models
+{
+    public class BikesRepository
+    {
+      
+            private string connectionstring;
+
+        public BikesRepository()
+            {
+                connectionstring = System.Configuration.ConfigurationManager.ConnectionStrings["connectionstring"].ConnectionString;
+            }
+
+            public List<Bike> GetAll(RequestModel request)
+            {
+                using (IDbConnection db = new SqlConnection(connectionstring))
+                {
+                    return db
+                        .Query<Bike>("Bikes_GetAll",
+                        request,
+                        commandType: CommandType.StoredProcedure)
+                        .ToList();
+                }
+            }
+            public Bike Get(int Id)
+            {
+                using (IDbConnection db = new SqlConnection(connectionstring))
+                {
+                    return db
+                            .Query<Bike>("Bikes_Get",
+                                new { Id },
+                                commandType: CommandType.StoredProcedure)
+                            .FirstOrDefault();
+                }
+            }
+            public int Create(Bike bike)
+            {
+                using (IDbConnection db = new SqlConnection(connectionstring))
+                {
+                    int lastInsertedId =
+                        db.Query<int>("Bikes_Create",
+                        bike,
+                        commandType: CommandType.StoredProcedure)
+                        .FirstOrDefault();
+                    return lastInsertedId;
+                }
+            }
+        public int Update(Bike bike)
+        {
+            using (IDbConnection db = new SqlConnection(connectionstring))
+            {
+                return db
+                    .Execute("Bikes_Update",
+                       bike,
+                       commandType: CommandType.StoredProcedure);
+            }
+        }
+        public int Delete(int Id)
+            {
+                using (IDbConnection db = new SqlConnection(connectionstring))
+                {
+                    return db.Execute(
+                            "Bikes_Delete",
+                            new { Id },
+                            commandType: CommandType.StoredProcedure);
+                }
+            }
+        }
+
+    }
